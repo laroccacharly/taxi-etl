43e4d91068cbc45ebaf8ed83c47ec998651b7f3b
Charly <larocca.charly@gmail.com>
2025-03-03 10:54:51 -0500

    deploy

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 alias gens="uv run print_structure.py"
 alias ui="uv run modal serve ui_modal.py"
 alias uil="APP_ENV=development uv run streamlit run ui.py"
+alias deploy="uv run modal deploy ui_modal.py"
